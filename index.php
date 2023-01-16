460f22abf759d624be7e7a5fbf00ff9f3351d5cb
Droduen <122769020+Droduen@users.noreply.github.com>
2023-01-16 01:12:39 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Home</title>
+    <link rel="icon" href="images/casper.png">
+    <link rel="stylesheet" href="code/style.css">
+  </head>
+  <body>
+   <?php echo "hi";?>
+  </body>
+</html>
